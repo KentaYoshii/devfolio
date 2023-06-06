97f5d2dd8897ac2551aa5abd2ed50c4639ca71fa
KentaYoshii <kenta_yoshii@brown.edu>
2023-06-07 08:59:25 +0900

    playing around

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -10,6 +10,7 @@ import { useTheme } from "@mui/material/styles";
 import me from "../assets/me.jpg";
 import me2 from "../assets/me2.jpg";
 import me3 from "../assets/me3.jpg";
+import me4 from "../assets/me4.jpg";
 
 const itemData = [
   {
@@ -24,28 +25,30 @@ const itemData = [
     img: me3,
     title: "me",
   },
+  {
+    img: me4,
+    title: "me",
+  },
 ];
 
 const Introduction = () => {
   const theme = useTheme();
   const size = useMediaQuery(theme.breakpoints.up("sm"));
   return (
-    <Box>
-      <Grid container>
-        <Grid item xs={12} md={6}>
-          <Box alignItems="center" justifyContent="center" display="flex" minHeight={size ? "40vh" : "0vh"}>
-            <Typography variant={size ? "h2" : "h6"}>
-              Hello!
-              My name is <br />
-              <strong>Kenta Yoshii</strong>
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{ width: "inherit", height: "inherit", overflowY: "scroll" }}
+    <Box minHeight="85vh">
+      <Box
+        minHeight="65vh"
+        justifyContent="center"
+        alignItems="center"
+        display="flex"
+      >
+        <Typography variant={size ? "h2" : "h3"}>
+          Hello! My name is <strong>Kenta Yoshii</strong>
+        </Typography>
+        <Box
+            sx={{ width: "30vw", height: "30vh", overflowY: "scroll", position: "absolute" }}
           >
-            <ImageList variant="masonry" cols={2} gap={8}>
+            <ImageList variant="masonry"  gap={8}>
               {itemData.map((item) => (
                 <ImageListItem key={item.img}>
                   <img
@@ -58,8 +61,25 @@ const Introduction = () => {
               ))}
             </ImageList>
           </Box>
-        </Grid>
-      </Grid>
+        {/* <Grid item xs={12} md={6}>
+          <Box
+            sx={{ width: "inherit", height: "inherit", overflowY: "scroll" }}
+          >
+            <ImageList variant="masonry"  gap={8}>
+              {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                  <img
+                    src={`${item.img}`}
+                    srcSet={`${item.img}`}
+                    alt={item.title}
+                    loading="lazy"
+                  />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Box>
+        </Grid> */}
+      </Box>
     </Box>
   );
 };
